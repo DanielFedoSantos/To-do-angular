c9567d67398247042127436d4f1a1c13213c9685
DanielFedoSantos <danielfedosantos@gmail.com>
2023-03-02 20:26:54 -0300

    criacao e estilizacao do header e footer, criacao das primeiras rotas

--- to-do/src/app/app-routing.module.ts
+++ to-do/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NewTaskComponent } from './components/pages/new-task/new-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'tasks/new', component: NewTaskComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

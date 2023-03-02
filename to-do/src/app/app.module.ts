f9e1faccd1aaaab5d01bcc3ee5e9b6a947f28f07
DanielFedoSantos <danielfedosantos@gmail.com>
2023-03-02 19:57:08 -0300

    cricao dos principais componentes e hello angular

--- to-do/src/app/app.module.ts
+++ to-do/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { NewTaskComponent } from './components/pages/new-task/new-task.component';
+import { EditTaskComponent } from './components/pages/edit-task/edit-task.component';
+import { DetailTaskComponent } from './components/pages/detail-task/detail-task.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormTaskComponent } from './components/form-task/form-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NewTaskComponent,
+    EditTaskComponent,
+    DetailTaskComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormTaskComponent
   ],
   imports: [
     BrowserModule,
---
+++ to-do/src/app/components/pages/detail-task/detail-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-task',
+  templateUrl: './detail-task.component.html',
+  styleUrls: ['./detail-task.component.css']
+})
+export class DetailTaskComponent {
+
+}
